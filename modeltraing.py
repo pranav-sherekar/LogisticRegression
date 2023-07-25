295d1880f299e54876f3827798da51a76100467e
shubham-khude <shubham.khude8066@gmail.com>
2023-07-25 15:27:19 +0530

    2 files added in modeltraining branch

---
+++ modeltraing.py
@@ -0,0 +1,5 @@
+print('working on model training ')
+
+print('noe we are in Model-traing branch')
+
+print('hii...give some comments from your side to check')
---
+++ modeltuning.py
@@ -0,0 +1,5 @@
+print('working on model training ')
+
+print('noe we are in Model-traing branch')
+
+print('my name is khan and am not a terrorist ')
